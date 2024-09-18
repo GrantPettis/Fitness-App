cdeb3514317a6a48c21bd1903741f26aba83a3a8
JamesPettis <jgpettis@crimson.ua.edu>
2024-09-18 15:13:17 -0500

    add exercise and sign in pages

---
+++ src/app/exercises/page.js
@@ -0,0 +1,5 @@
+export default function exercises_page(){
+    return <main>
+        <h1>exercises_page</h1> 
+    </main>
+}
---
+++ src/app/sign_in/page.js
@@ -0,0 +1,3 @@
+export default function sign_in_page(){
+    return <h1>sign_in_page</h1>
+}
