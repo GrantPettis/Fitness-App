0990e27f3f8e06bce01ce622cd4bc4eb2a95a0b7
JamesPettis <jgpettis@crimson.ua.edu>
2024-10-02 15:04:38 -0500

    Work on getting the exercises on the page

--- src/app/exercises/page.js
+++ src/app/exercises/page.js
@@ -1,6 +1,39 @@
+"use client"
+import { Firestore, getDoc, collection, getDocs, QuerySnapshot } from 'firebase/firestore';
 import Link from 'next/link';
+import {useCollection} from "react-firebase-hooks/firestore"
+import {db} from '@/app/firebase/firebase'; 
+import { useEffect, useState } from 'react';
+
+
+async function fetchdatafromFirestore() {
+    const QuesrySnapshot = await getDocs(collection(db,"Exercises"))
+
+    const data = [];
+    QuerySnapshot.array.forEach(doc => {
+        data.push({id: doc.id, ...doc.data()});
+    });
+    return data;
+}
 export default function exercises_page(){
+
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            const data = await fetchdatafromFirestore();
+            setData(data);
+            
+        }
+        fetchData();
+    },[]);
     return <main>
         <h1>exercises_page</h1> 
+        <div>
+            {data.map((Exercise)=> (
+                <div key={Exercise.id} className='mb-4'>
+                    <p>{Exercise.name}</p>
+                    </div>
+            ))} 
+        </div>
     </main>
 }
