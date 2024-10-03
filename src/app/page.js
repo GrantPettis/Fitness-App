f5f1e130712711bcae000e60b42af373fb2b218d
JamesPettis <jgpettis@crimson.ua.edu>
2024-10-03 14:11:21 -0500

    Logo and tagline on the homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from 'next/link';
+import logoImg from '@/app/assets/Logo.PNG'
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <ol>
+          <Image className = {styles.home}  src={logoImg} alt='UBM logo'></Image>
+          <p className={styles.page}>Change your life, One step at a time</p>
         </ol>
 
         <div className={styles.ctas}>
